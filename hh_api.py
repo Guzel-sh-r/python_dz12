55ac6f0832dc169f15abd0d7dfcfd43b5fad8b0b
Guzel <guzel-sh-r@yandex.ru>
2023-08-29 03:10:17 +0300

    add hh html/css Bootstrap

--- hh_api.py
+++ hh_api.py
@@ -1,7 +1,7 @@
 import requests
 import pprint
 import json
-import re
+import time
 
 
 url = 'https://api.hh.ru/vacancies'
@@ -29,17 +29,15 @@ def func_hh(num, text):
 text = input("Введите интересующую вакансию: ")
 
 result = requests.get(url, params={'text': text, 'page': 0}).json()
-
+# pprint.pprint(result)
 items = result["items"]
 pages = result["pages"]
 per_page = result["per_page"]
 found = result["found"]
 
-all_quantity = pages * per_page
-if found >= all_quantity:
-    answer_count = all_quantity
-else:
-    answer_count = per_page
+answer_count = 0
+if pages >= 5:
+    pages = 5
 
 result = {'keywords': text,
           'count': answer_count,
@@ -51,52 +49,52 @@ all_snippet, all_salary = {}, []
 # pprint.pprint(items)
 
 for p in range(pages):
+    time.sleep(5)
     items = func_hh(p, text)
     print(f"Страница №{p}")
     for i in range(len(items)):
         snippet = set()
-        val1 = items[i]["snippet"]["requirement"]
-        val2 = items[i]["snippet"]["responsibility"]
-        if val1 is not None:
-            val1 = val1.replace("<highlighttext>Python</highlighttext>", "Python")
-            snippet1 = re.findall(r'\s[A-Za-z-?]+', val1)
-            snippet11 = [j.lower().strip() for j in snippet1]
-            snippet.update(snippet11)
+        url_vacancy = items[i]["url"]
+        result_vacancy = requests.get(url_vacancy).json()
+        # pprint.pprint(result_vacancy)
 
-        if val2 is not None:
-            snippet2 = re.findall(r'\s[A-Za-z-?]+', val2)
-            snippet22 = [j.lower().strip() for j in snippet2]
-            snippet.update(snippet22)
+        key_skills = result_vacancy["key_skills"]
 
-        snippet.discard("-")
+        if len(key_skills) > 0:
+            for key_skill in key_skills:
+                snippet.add(key_skill["name"])
+            for val in snippet:
+                if val[-1] == "-":
+                    val0 = val[:-1]
+                else:
+                    val0 = val
+                if val0 in all_snippet:
+                    all_snippet[val0] += 1
+                else:
+                    all_snippet[val0] = 1
 
-        for val in snippet:
-            if val[-1] == "-":
-                val0 = val[:-1]
+            salary = result_vacancy["salary"]
+            if salary is not None:
+                curr = salary["currency"]
+                salary_from = salary["from"]
+                salary_to = salary["to"]
+                if salary_from is None and salary_to is None:
+                    salary = 0
+                elif salary_to is None and salary_from is not None:
+                    salary = salary_from
+                elif salary_from is None and salary_to is not None:
+                    salary = salary_to
+                else:
+                    salary = (int(salary_from) + int(salary_to) / 2)
+
+                if curr != "RUR":
+                    salary = func_converter_curr(curr, salary)
             else:
-                val0 = val
-            if val0 in all_snippet:
-                all_snippet[val0] += 1
-            else:
-                all_snippet[val0] = 1
+                salary = 0
 
-        salary = items[i]["salary"]
-        if salary is not None:
-            curr = salary["currency"]
-            if salary["from"] is None:
-                salary = salary["to"]
-            elif salary["to"] is None:
-                salary = salary["from"]
-            else:
-                salary = int((salary["to"] + salary["from"]) / 2)
-        else:
-            salary = 0
-            curr = "RUR"
+            all_salary.append(salary)
 
-        if curr != "RUR":
-            salary = func_converter_curr(curr, salary)
-
-        all_salary.append(salary)
+            answer_count += 1
 
 result["salary_average"] = int(sum(all_salary) / len(all_salary))
 
@@ -113,6 +111,8 @@ for name, count in all_snippet.items():
 [add_sort.append(i) for i in sorted(add, key=lambda x: x['count'], reverse=True)]
 
 result["requirements"] = add_sort
+
+result["count"] = answer_count
 
 with open("result.json", mode="w") as f:
     json.dump(result, f)
